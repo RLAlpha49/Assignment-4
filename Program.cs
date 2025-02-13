53fc5b62ca38da057c5b9b18458dbb95551a334d
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2025-02-13 13:29:40 -0600

    Add character name

--- Program.cs
+++ Program.cs
@@ -56,7 +56,18 @@ else
         logger.Info("User choice: {Choice}", choice);
         if (choice == "1")
         {
-            // Add Character
+            Console.WriteLine("Enter new character name: ");
+            string? Name = Console.ReadLine();
+            if (!string.IsNullOrEmpty(Name))
+            {
+                // generate id - use max value in Ids + 1
+                UInt64 Id = Ids.Max() + 1;
+                Console.WriteLine($"{Id}, {Name}");
+            }
+            else
+            {
+                logger.Error("You must enter a name");
+            }
         }
         else if (choice == "2")
         {
