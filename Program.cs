51504fc27799ad818ad92c09a33915d68822f27a
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2025-02-13 13:22:42 -0600

    Create logger instance

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+logger.Info("Program started");
+Console.WriteLine("Hello World!");
+logger.Info("Program ended");
