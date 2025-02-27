981c1caca4f97e1bc498ca1980cad19b47c60dd3
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2025-02-27 13:20:10 -0600

    Use a single list of MarioCharacter instead of multiple lists

--- Program.cs
+++ Program.cs
@@ -13,12 +13,7 @@ if (!File.Exists(file))
 }
 else
 {
-    List<UInt64> Ids = [];
-    List<string> Names = [];
-    List<string?> Descriptions = [];
-    List<string?> Species = [];
-    List<string?> FirstAppearances = [];
-    List<int?> YearCreated = [];
+    List<MarioCharacter> characters = new List<MarioCharacter>();
 
     try
     {
@@ -30,20 +25,17 @@ else
             string? line = sr.ReadLine();
             if (line is not null)
             {
-                // character details are separated with comma(,)
                 string[] characterDetails = line.Split(',');
-                // 1st array element contains id
-                Ids.Add(UInt64.Parse(characterDetails[0]));
-                // 2nd array element contains character name
-                Names.Add(characterDetails[1]);
-                // 3rd array element contains character description
-                Descriptions.Add(characterDetails[2]);
-                // 4th array element contains character species
-                Species.Add(characterDetails[3]);
-                // 5th array element contains character first appearance
-                FirstAppearances.Add(characterDetails[4]);
-                // 6th array element contains character year created
-                YearCreated.Add(int.Parse(characterDetails[5]));
+
+                ulong id = UInt64.Parse(characterDetails[0]);
+                string name = characterDetails[1];
+                string? description = characterDetails[2];
+                string? species = characterDetails[3];
+                string? firstAppearance = characterDetails[4];
+                int? yearCreated = int.Parse(characterDetails[5]);
+
+                MarioCharacter character = new MarioCharacter(id, name, description, species, firstAppearance, yearCreated);
+                characters.Add(character);
             }
         }
         sr.Close();
